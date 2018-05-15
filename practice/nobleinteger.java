4b34db57105af0fa2c401a7b45830c6004f40280
sapnaavya <sapnapg@clemson.edu>
2018-05-14 21:11:50 -0700

    noble integer

---
+++ practice/nobleinteger.java
@@ -0,0 +1,31 @@
+public class NobleInteger{
+
+    public static void main (String args[]){
+        int[] arr = {10,20,3,40,2}
+        int res = NobleInteger(arr);
+        if(res != -1){
+            System.out.println("the noble integer " + res);
+        }
+        else{
+            System.out.println("no noble integer found");
+        }
+    }
+    
+    public static NobleInteger(int arr[]){
+        Arrays.sort(arr);
+        int n = arr.length;
+        for (int i = 0; i <= n-1; i++){
+            if(a[i] == a[i+1]){
+                continue;
+            }
+            if(arr[i] == n-i-1){
+                return arr[i];
+            }
+        }
+        
+        if(arr[n-1] == 0){
+            return arr[n-1];
+        }
+    return n-1;
+    }
+}
