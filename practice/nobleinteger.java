9c3686f4fcd1528d98b4321647a3966f7fc35f66
Sapna Gandhi (Terawe Corporation) <v-sapgan@microsoft.com>
2018-10-13 18:42:40 -0700

    test

--- practice/nobleinteger.java
+++ practice/nobleinteger.java
@@ -1,7 +1,13 @@
+// Find the number of integer greater than or equal to a value
+
+import java.lang.Math; // headers MUST be above the first class
+import java.util.*;
+// one class needs to have a main() method
 public class NobleInteger{
     public static void main (String args[]){
-        int[] arr = {10,20,3,40,2}
-        int res = NobleInteger(arr);
+       // int[] arr = {1,2,1,3};
+       int[] arr = {10,20,3,40,2};
+        int res = findNoble(arr);
         if(res != -1){
             System.out.println("the noble integer " + res);
         }
@@ -10,11 +16,11 @@ public class NobleInteger{
         }
     }
 
-    public static NobleInteger(int arr[]){
+    public static int findNoble(int arr[]){
         Arrays.sort(arr);
         int n = arr.length;
-        for (int i = 0; i <= n-1; i++){
-            if(a[i] == a[i+1]){
+         for (int i = 0; i <= n-1; i++){
+            if(arr[i] == arr[i+1]){
                 continue;
             }
             if(arr[i] == n-i-1){
@@ -25,6 +31,7 @@ public class NobleInteger{
         if(arr[n-1] == 0){
             return arr[n-1];
         }
-    return n-1;
+      return n-1;
     }
+  
 }
