30fbd7f5706cd2778025cf4a1f8505f7ae236276
Sapna Gandhi (Terawe Corporation) <v-sapgan@microsoft.com>
2018-10-13 23:04:31 -0700

    adding main methode in URLIFY Code

---
+++ CrackingTheCodingInterview/ch1/replaceSpacewith%203.java
@@ -0,0 +1,35 @@
+
+public class Solution{
+
+// Write a method to replace all spaces in a string with '%20'.
+	// COMPLEXITY O (N + 2 * #SPACES ) O (N)  AVG
+public static char[] replaceSpaces(char str[]){
+	int spaces = 0;
+  System.out.println(str.length);
+	for(int i = 0; i < str.length; i++){
+		if(str[i] == ' '){
+			spaces++;
+		}
+	}
+	int length = str.length + (2*spaces);
+	char result [] = new char[length];
+	int index = 0;
+	for(int i = 0; i < str.length; i++){
+		if(str[i] == ' '){
+			result[index++] = '%';
+			result[index++] = '2';
+			result[index++] = '0';
+		}else{
+			result[index++] = str[i];
+		}
+	}
+	return result;
+}
+
+    public static void main(String args[]){
+        char[] charArr = {'s','a',' ','p', ' '};
+            System.out.println(replaceSpaces(charArr));
+
+    }
+
+}
