a027f74aa120701c2dc038fa26620075ae1b8cf1
unknown <Admin@SAPGAN.redmond.corp.microsoft.com>
2019-02-22 10:36:41 -0800

    Parameterized duplicate character

--- Arrays/FirstDuplicateCharacter.java
+++ Arrays/FirstDuplicateCharacter.java
@@ -1,12 +1,22 @@
 import java.util.Map;
 import java.util.HashMap;
-class Ideone
+import java.util.Scanner;
+import java.util.Arrays;
+class FirstDuplicateCharacter
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int[] intArray = {5, 4, 6, 8, 9};
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enther the length of the array");
+		int length = sc.nextInt();
+		int [] myArray = new int[length];
+		System.out.println("Enther the elements of the array");
+		for(int i=0; i<length; i++){
+			myArray[i] = sc.nextInt();
+		}
+		System.out.println(Arrays.toString(myArray));
 		Map<Integer, Integer> freqCountMap = new HashMap<Integer, Integer>();
-		for(int a : intArray) {
+		for(int a : myArray) {
 			Integer k = freqCountMap.get(a);
 			if(k != null) {
 				System.out.println("First duplicate occurrence is for " + a);
