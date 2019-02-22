5612e8ceb610c708b9b9cbe1c6ec9b038c74a1ef
unknown <Admin@SAPGAN.redmond.corp.microsoft.com>
2019-02-22 10:42:49 -0800

    fixing typo

--- Arrays/FirstDuplicateCharacter.java
+++ Arrays/FirstDuplicateCharacter.java
@@ -7,10 +7,10 @@ class FirstDuplicateCharacter
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner (System.in);
-		System.out.println("Enther the length of the array");
+		System.out.println("Enter the length of the array");
 		int length = sc.nextInt();
 		int [] myArray = new int[length];
-		System.out.println("Enther the elements of the array");
+		System.out.println("Enter the elements of the array");
 		for(int i=0; i<length; i++){
 			myArray[i] = sc.nextInt();
 		}
