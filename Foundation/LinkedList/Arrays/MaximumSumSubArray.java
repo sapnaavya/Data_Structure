2b87cbe307232e55d642e29110ddb2265be9002d
unknown <Admin@SAPGAN.redmond.corp.microsoft.com>
2019-05-14 21:29:07 -0700

    adding file to find maximum sum of a subarray

---
+++ Foundation/LinkedList/Arrays/MaximumSumSubArray.java
@@ -0,0 +1,48 @@
+/* A java program to find maximum sum in a subArray*/
+
+import java.util.*;
+public class MaximumSumSubArray {
+    
+    public void maxSumSubArray (int [] arr) {
+        int start = 0;
+        int end = 0;
+        int s = 0;
+        int i;
+        if(arr == null || arr.length == 0) {
+            return;
+        }
+        int maxSofar = arr[0];
+        int maxSum = 0;
+        for(i=0; i<arr.length; i++) {
+            maxSum = maxSum + arr[i];
+            if(maxSofar < maxSum){
+                maxSofar = maxSum;
+                start = s;
+                end = i;
+            }
+    
+            if(maxSum < 0) {
+                maxSum = 0;
+                s = i+1;
+            }   
+        }
+        System.out.println("The maximum sum of subarray" + maxSum);
+        for(int j  = start; j <= end; j++) {
+            System.out.println(arr[j] + " ");
+        }
+
+    }
+    //Main method
+    public static void main(String args[]) {
+       MaximumSumSubArray maxSum = new MaximumSumSubArray();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the array");
+        int length = sc.nextInt();
+        int [] arr = new int[length];
+        for(int i=0; i<length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        maxSum.maxSumSubArray(arr);
+        sc.close();
+    }
+}
