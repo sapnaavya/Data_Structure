48b9f7a286c92903500f79d8af6c08fda17b42d2
unknown <Admin@SAPGAN.redmond.corp.microsoft.com>
2019-06-01 22:13:28 -0700

    adding code to check if a string is a pallindrome or not

---
+++ Foundation/Strings/PallindromeCheck.java
@@ -0,0 +1,33 @@
+/* This code check if a string is a pallindromic string or not.
+
+    brute force approach would be create a String build object and reverse the content of the string and store it in a another string object.
+    In this approach, we are checking first and last element of string
+*/
+import java.util.Scanner;
+
+public class PallindromeCheck {
+
+    //Check if a string is pallindrome or not
+    public boolean isPallindrome(String str) {
+        if(str == null) {
+            return true;
+        }
+        int length = str.length();
+        for(int i=0; i< length/2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(length - i - 1);
+            if(start != end) {
+                return false;
+            }
+        }
+        return true;
+    }
+    //main method
+    public static void main(String args[]) {
+        PallindromeCheck palObj = new PallindromeCheck();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        boolean isPal = palObj.isPallindrome(str);
+        System.out.println(isPal);
+    }
+}
