1dc4b462601daa71bc770ccf1b92be18484904df
sapna gandhi <sapnapg@clemson.edu>
2019-06-22 12:29:16 -0700

    fixing bug

--- Foundation/Strings/FindPattern.java
+++ Foundation/Strings/FindPattern.java
@@ -7,7 +7,12 @@ public class FindPattern {
 
     //Find the starting index of a pattern in a string
     public static int findPattern(String str, String subsetString) {
-        if(str == null || subsetString == null) {
+        if(str == null || subsetString == null || str.length() == 0 || subsetString.length() == 0) {
+            return -1;
+        }
+
+        //subset string can not have length greater than string
+        if(subsetString.length() > str.length()) {
             return -1;
         }
 
@@ -16,20 +21,33 @@ public class FindPattern {
         char[] charArr2 = subsetString.toCharArray();
         int s = 0;
         int start = s;
-        for(int i=0; i<charArr1.length; i++) {
+        //for(int i=0; i<charArr1.length; i++) { //DO NOT USE THIS FOR LOOP AS IT INCREMENTS i AND WE DO NOT WANT TO INCREMENT I EVERYTIME TO AVOID BLIND SPOT
+        
+        /**
+         * make sure to reset the k pointer..before incrementing i .. thus first else if and check if k != 0  and if k is 0 then increment i++ to move on with search
+         * to avoid this scenario: for example, string is sappna and subset string is pna 
+        */
+
+        int i = 0;
+        while(i < charArr1.length) {       
             if(charArr1[i] == charArr2[k]) {
                 start = s;
                 k++;
-                if(k == subsetString.length()) {
+                if(k == subsetString.length()) { // if k is equal to subset length that mean we have traversed the whole subset and found the subset
                     return start;
-                } 
-                continue;
+                }
+                i++; 
+                continue; //make sure not to miss this continue
             } 
-            else if(k != 0) {
+            else if(k != 0) { // In this case, do not increment i as we want to compare with k as 0 with existing i and if they do not match then increment i
                 k = 0;
+                s = i + 1;
+                continue;// make sure to use this continue as we do not want increment i after resetting k to 0
             }
+            i++;
             s = i + 1;
         }
+        //}
         return -1;
     }
 
