a149532bf2be8046c73f7731c77c80f3f53d6ddf
Sapna Gandhi <sapnapg@clemson.edu>
2019-09-07 12:25:00 -0700

    adding code for printing all parenthesis

---
+++ Foundation/NewProblems/LongWeekendProblems/Set6/PrintAllParenthesis.java
@@ -0,0 +1,40 @@
+/**
+ * This code prints all possible combinations of parenthesis
+ * Reference: https://www.geeksforgeeks.org/print-all-combinations-of-balanced-parentheses/
+*/
+
+public class PrintAllParenthesis {
+    public static void printParenthesis(char str[], int pos, int n, int open, int close) {
+        if(close == n) {
+            for(int i = 0; i < str.length; i++) {
+                System.out.print(str[i]);
+            }
+            System.out.println();
+            return;
+        }
+        else {
+            if(open > close) {
+                str[pos] = '}';
+                printParenthesis(str, pos+1, n, open, close+1);
+            }
+            if(open < n) {
+                str[pos] = '{';
+                printParenthesis(str, pos+1, n, open+1, close);
+            }
+        }
+    }
+
+    public static void printParenthesis(char[] str, int n) {
+        if(n > 0) {
+            printParenthesis(str, 0, n, 0, 0);
+        }
+        return;
+    }
+
+    // main method
+    public static void main(String args[]) {
+        int n = 3;
+        char[] str = new char[2 * n];
+        printParenthesis(str, n);
+    }
+}
