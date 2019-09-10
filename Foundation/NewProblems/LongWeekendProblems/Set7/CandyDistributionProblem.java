9efb3c0f371194fd05ccba58df4e1e3f19cc4ada
Sapna Gandhi <sapnapg@clemson.edu>
2019-09-09 22:48:03 -0700

    adding code for candy distribution

--- Foundation/NewProblems/LongWeekendProblems/Set7/CandyDistributionProblem.java
+++ Foundation/NewProblems/LongWeekendProblems/Set7/CandyDistributionProblem.java
@@ -13,13 +13,13 @@ public class CandyDistributionProblem {
         Arrays.fill(candies, 1);
         for(int i = 1; i < n; i++) {
             if(ratings[i] > ratings[i-1]) {
-                candies[i] = Math.max(candies[i], candies[i-1] + 1);
+                candies[i] = candies[i-1] + 1;
             }
         }
     
         for(int i = n-2; i >= 0; i--) {
             if(ratings[i] > ratings[i+1]) {
-                candies[i] = Math.max(candies[i+1] + 1, candies[i]);
+                candies[i] += 1;
             }
         }
 
@@ -33,7 +33,8 @@ public class CandyDistributionProblem {
 
     // main method
     public static void main(String args[]) {
-       int [] arr = {1, 2, 1, 5};
+       int [] arr = {1,2,1,4};
+       //int[] arr = {1, 5, 2, 1};
        int minCandies = candiesDistribution(arr);
        System.out.println(minCandies);
     }
