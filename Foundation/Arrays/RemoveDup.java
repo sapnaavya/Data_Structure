97e19a8f8ab8b1aa7ace163f7749a7f4151b02bb
Sapna Gandhi <sapnapg@clemson.edu>
2019-09-29 10:46:59 -0700

    moving files to right folder

--- Foundation/Arrays/RemoveDup.java
+++ Foundation/Arrays/RemoveDup.java
@@ -96,15 +96,15 @@ public class RemoveDup {
         //     System.out.println(arr[i]);
         // }
 
-        int len1 = removeDuplicateElementsSorted(arr, arr.length);
-        for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-
-        // int n = removeDupArr(arr1);
-        // for(int i = 0; i < n; i++) {
-        //     System.out.println(arr1[i]);
+        // int len1 = removeDuplicateElementsSorted(arr, arr.length);
+        // for(int i = 0; i < arr.length; i++) {
+        //     System.out.println(arr[i]);
         // }
+
+        int n = removeDupArr(arr1);
+        for(int i = 0; i < n; i++) {
+            System.out.println(arr1[i]);
+        }
 
     }
 }
